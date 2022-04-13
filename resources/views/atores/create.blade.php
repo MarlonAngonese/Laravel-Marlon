c240a5ff238a77c9b00f71b41081b8c8d970e271
MarlonAngonese <marlonmaster2020@gmail.com>
2022-04-13 19:54:40 -0300

    first commit

--- resources/views/atores/create.blade.php
+++ resources/views/atores/create.blade.php
@@ -8,5 +8,16 @@
             {!! Form::label('nome', 'Nome:') !!}
             {!! Form::text('nome', null, ['class' => 'form-control', 'required']) !!}
         </div>
+        <div class="form-group">
+            {!! Form::label('nacionalidade', 'Nacionalidade:') !!}
+            {!! Form::select('nacionalidade', [
+                'BRA' => 'Brasileiro',
+                'USA' => 'Americano',
+                'CAN' => 'Canadense',
+                'ARG' => 'Argentino',
+                'BRA',
+                ['class' => 'form-control', 'required']
+            ]) !!}
+        </div>
     {!! Form::close() !!}
 @stop
