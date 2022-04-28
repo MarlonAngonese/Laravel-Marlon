cd896a8876ea27ce6ce5cfac3177e48e77e7c4cf
MarlonAngonese <marlonmaster2020@gmail.com>
2022-04-27 22:00:01 -0300

    27-abr-2022

--- database/seeders/NacionalidadeSeeder.php
+++ database/seeders/NacionalidadeSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Nacionalidade;
 
 class NacionalidadeSeeder extends Seeder
 {
@@ -13,6 +14,20 @@ class NacionalidadeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Nacionalidade::create(['descricao' => 'Brasileiro(a)']);
+        Nacionalidade::create(['descricao' => 'Argentina(a)']);
+        Nacionalidade::create(['descricao' => 'Norte-Americano(a)']);
+        Nacionalidade::create(['descricao' => 'Canadense']);
+        Nacionalidade::create(['descricao' => 'Uruguaio(a)']);
+        Nacionalidade::create(['descricao' => 'Espanhol(a)']);
+        Nacionalidade::create(['descricao' => 'Ingles(a)']);
+        Nacionalidade::create(['descricao' => 'Peruano(a)']);
+        Nacionalidade::create(['descricao' => 'Colombiano(a)']);
+        Nacionalidade::create(['descricao' => 'Japones(a)']);
+        Nacionalidade::create(['descricao' => 'Chines(a)']);
+        Nacionalidade::create(['descricao' => 'Sul-Africano(a)']);
+        Nacionalidade::create(['descricao' => 'Polones(a)']);
+        Nacionalidade::create(['descricao' => 'Alemã(o)']);
+        Nacionalidade::create(['descricao' => 'Russo(a)']);
     }
 }
